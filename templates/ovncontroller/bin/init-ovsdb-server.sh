12df15867e215182cf1c85fc1d1b58947c2efb42
Arnau Verdaguer <averdagu@redhat.com>
2025-02-20 17:09:15 +0100

    [ovn-controller] Change startup mechanism of ovs pods
    This commit aims to modify the starting scripts of the
    ovn-controller-ovs daemonset.
    
    This is done to allow modifying the RollingUpdate Strategy
    to not allow any Unavailable pod during update, what this will
    cause is that during an update to the pod (delete old one and create
    new one) instead of first deleting the first one and then deleting the
    next one, it will create the new pod while the old one is running. Due
    to how the startup scritps works currently this is not allowed.
    
    The reason behind this is to try to lower the downtime observed if the
    environment is using centralized floating ip during an update.
    
    With this commit the ovn-controller-ovs will share the PID namespace
    with the host, in order to allow signaling between old/new pod.
    
    Another change is adding an STATE that the containers (ovsdb-server,
    ovs-vswitchd and ovsdb-server-init) will handle internally.
    
    The differents states are:
     - NULL (No file): will happen the fist time ds is created on the oc
       worker.
     - INIT: First time init-ovsdb-server is executed on the oc worker.
     - OVSDB_SERVER: once ovsdb-server pod has run the startup script
     - RUNNING: Once ovsdb-server is up  and ovs-vswitchd has run the
       startup script.
     - UPDATE: Once a new pod is created and ovsdb-server-init has run.
     - RESTART_VSWITCH: After ovsdb-server-init has finished, new
       ovsdb-server pod has stopped the old ovs-vswitchd process.
     - RESTART_DBSERVER: After old ovs-vswitchd has been restarted the
       old ovsdb-server is also stop.
    
    The normal flow of states is the following:
    
    NULL -> INIT -> OVSDB_SERVER -> RUNNING
    
    Scale down: If the oc worker is deleted the DS and all the pods and mount points
    will be deleted, in case of node being up again it should start from
    NULL
    
    Update: RUNNING -> (Change on CR) -> UPDATE -> RESTART_VSWITCHD ->
    RESTART_DBSERVER -> OVSDB_SERVER -> RUNNING
    
    Related: OSPRH-11636
    Jira: OSPRH-10821

--- templates/ovncontroller/bin/init-ovsdb-server.sh
+++ templates/ovncontroller/bin/init-ovsdb-server.sh
@@ -14,19 +14,51 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+# Configs are obtained from ENV variables
+isUpdate=${isUpdate:-"false"}
+
 set -ex
 source $(dirname $0)/functions
 trap wait_for_db_creation EXIT
+
+sleep 3
+TLSOptions="--certificate=/etc/pki/tls/certs/ovndb.crt --private-key=/etc/pki/tls/private/ovndb.key --ca-cert=/etc/pki/tls/certs/ovndbca.crt"
+DBOptions="--db ssl:ovsdbserver-nb.openstack.svc.cluster.local:6641"
 
 # If db file is empty, remove it; otherwise service won't start.
 # See https://issues.redhat.com/browse/FDP-689 for more details.
 if ! [ -s ${DB_FILE} ]; then
     rm -f ${DB_FILE}
 fi
+
+# Check if file is created, if not means it's first execution
+if [ -f /var/lib/openvswitch/already_executed ]; then
+  # File is created, no need to run ovs-ctl
+  # Change state to "UPDATE"
+  echo "UPDATE" > /var/lib/openvswitch/already_executed
+  ## Need to lower chassis priority
+  # First get the system-id
+  chassis_id=$(ovs-vsctl get Open_Vswitch . external_ids:system-id)
+  nb_output=$(ovn-nbctl $DBOptions $TLSOptions --columns=_uuid,priority find Gateway_Chassis chassis_name=$chassis_id)
+  row_uuid=$(echo "$nb_output" | grep "_uuid" | cut -d':' -f2 | xargs)
+  priority=$(echo "$nb_output" | grep "priority" | cut -d':' -f2 | xargs)
+  # Save priority to be able to restore it later (It's overwritting, not appending, hence no check)
+  echo $priority > /var/lib/openvswitch/old_priority
+  # Set lower priority (lowest value possible 0)
+  ovn-nbctl $DBOptions $TLSOptions set Gateway_Chassis $row_uuid priority=0
+  sleep 10
+  exit 0
+fi
+
 # Initialize or upgrade database if needed
 CTL_ARGS="--system-id=random --no-ovs-vswitchd"
 /usr/share/openvswitch/scripts/ovs-ctl start $CTL_ARGS
 /usr/share/openvswitch/scripts/ovs-ctl stop $CTL_ARGS
+
+if [ ! -f /var/lib/openvswitch/already_executed ]; then
+  # If file was not present, set status INIT
+  echo "INIT" > /var/lib/openvswitch/already_executed
+fi
 
 wait_for_db_creation
 trap - EXIT
